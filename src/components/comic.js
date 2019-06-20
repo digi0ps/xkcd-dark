f3bbdaee675aa17614fbab99217bc8e26256a2c2
jeremyphilemon <jeremyphilemon@outlook.com>
2019-06-20 18:14:57 +0300

    Updated routing logic.

--- src/components/comic.js
+++ src/components/comic.js
@@ -2,61 +2,58 @@ import React, { Component } from 'react';
 import './../App.css';
 
 import { withRouter } from 'react-router-dom';
+import ContentLoader from 'react-content-loader'
+
+const MyLoader = () => (
+  <ContentLoader primaryColor={"#1b1b1b"} secondaryColor={'#1e1e1e'} speed={1.25} width={400} height={190}>
+    <rect x="142" y="5" rx="5" ry="5" width="115" height="30" />
+    <rect x="125" y="47" rx="4" ry="4" width="149" height="140" />
+  </ContentLoader>
+)
 
 class Comic extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comic: {img: ''},
+      shouldUpdate: false,
+      loaded: false,
     }
   }
 
-  fetchComic = (number) => {
-  	fetch(`https://xkcd.now.sh/${number}`).then((response) => {
-      response.json().then((parsedJSON) => {
-        const comic = parsedJSON;
-        let url = comic.img;
-        if(comic.hasOwnProperty('imgRetina')) {
-          url = comic.imgRetina;
-        }
-        this.setState({
-          comic: comic,
-          url: url,
-          shouldUpdate: false,
-        }, ()=> {
-          this.props.sendNumberToParent(comic.num);
-          this.props.history.push(`/${comic.num}`)
-        })
-      })
-    }).catch(console.log("Could not fetch the comic :/"))
+  imageHasLoaded = () => {
+    this.setState({
+      loaded: true,
+      shouldUpdate: false,
+    })
   }
 
   componentDidMount() {
-    this.fetchComic(this.props.number)
   }
 
   componentWillReceiveProps(nextProps) {
   	if(this.props.number!==nextProps.number) {
   		this.setState({
   			shouldUpdate: true,
+        loaded: false,
   		})
   	}
   }
 
   componentDidUpdate() {
-    if(this.state.shouldUpdate===true) {
-      this.fetchComic(this.props.number)
+    if(this.state.shouldUpdate===true && this.props.shouldUpdateLock===false) {
+      this.props.updateComicFn(this.props.number)
     }
   }
 
   render() {
     return (
-        <div className="">
-          <div className="title">{this.state.comic.title}</div>
-          <img alt={this.state.comic.alt} src={this.state.url}/>
+        <div className="comic-wrapper">
+          <div style={{display: this.state.loaded ? 'none' : 'initial'}}><MyLoader className={'loader'}/></div>
+          <div className="title" style={{display: !this.state.loaded ? 'none' : 'initial'}}>{this.props.comic.title}</div>
+          <img alt={this.props.comic.alt} src={this.props.url} style={{visibility: !this.state.loaded ? 'hidden' : 'visible'}} onLoad={this.imageHasLoaded}/>
         </div>
     );
   }
 }
 
-export default withRouter(Comic);
+export default withRouter(Comic)
--- src/components/home.js
+++ src/components/home.js
@@ -7,37 +7,76 @@ import { withRouter } from 'react-router-dom';
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.updateComic = this.updateComic.bind(this)
     this.state = {
+      comic: {img: '', num: -1},
       number: this.props.match.params.number ? this.props.match.params.number : '',
+      shouldUpdateLock: true,
     }
-    this.getLatestNumber = this.getLatestNumber.bind(this);
+  }
+
+  fetchComic = (number) => {
+    fetch(`https://xkcd.now.sh/${number}`).then((response) => {
+      response.json().then((parsedJSON) => {
+        const comic = parsedJSON;
+        let url = comic.img;
+        if(comic.hasOwnProperty('imgRetina')) {
+          url = comic.imgRetina;
+        }
+        this.setState({
+          comic: comic,
+          url: url,
+          shouldUpdateLock: true,
+        }, ()=> {
+          this.props.history.push(`/${comic.num}`);
+        })
+      })
+    }).catch()
   }
 
   componentDidMount() {
-
-  }
-
-  getLatestNumber = (latestNumber) => {
-    this.setState({
-      number: latestNumber,
+    this.fetchComic(this.state.number)
+    fetch("https://xkcd.now.sh/").then((response) => {
+      response.json().then((parsedJSON) => {
+        this.setState({
+          upper: parsedJSON.num,
+        })
+      })
     })
   }
 
+  updateComic = (number) => {
+    this.fetchComic(number)
+  }
+
   navigate = (option) => {
-    if(option===0 && this.state.number>1) {
+    if(option===42) {
+      const random = Math.floor(Math.random() * (this.state.upper - 1 + 1) ) + 1;
       this.setState({
-        number: this.state.number-1,
+        number: random,
+        shouldUpdateLock: false,
+      })
+    }
+    else if(option===0 && this.state.number>=1) {
+      this.setState({
+        number: this.state.number===1? this.state.number : this.state.number-1,
+        shouldUpdateLock: false,
       })
     } else if(option===-100) {
       this.setState({
         number: 1,
+        shouldUpdateLock: false,
       })
     } else if(option===100) {
-      this.props.history.push('/');
+      this.setState({
+        number: this.state.upper,
+        shouldUpdateLock: false,
+      })
     }
     else {
       this.setState({
-        number: this.state.number+1,
+        number: this.state.number+1 > this.state.upper ? this.state.upper : this.state.number+1,
+        shouldUpdateLock: false,
       })
     }
   }
@@ -45,14 +84,17 @@ class Home extends Component {
   render() {
     return (
       <div className="home is-unselectable">
+        <p className="credit">All comics displayed here are Randall Munroe's creations, obtained from <a href="https://xkcd.com">xkcd.com</a><br/>
+        For bug fixes/feature requests, head over to the <a href="https://github.com/jeremyphilemon/xkcd-dark">repository</a> here.</p>
         <div className="navigation">
           <button className="navigation-button" onClick={this.navigate.bind(null, -100)}>[First]</button>
           <button className="navigation-button" onClick={this.navigate.bind(null, 0)}>[Prev]</button>
+          <button className="navigation-button" onClick={this.navigate.bind(null, 42)}>[Random]</button>
           <button className="navigation-button" onClick={this.navigate.bind(null, 1)}>[Next]</button>
           <button className="navigation-button" onClick={this.navigate.bind(null, 100)}>[Latest]</button>
         </div>
         <div className="comic">
-          <Comic number={this.state.number} sendNumberToParent={this.getLatestNumber}/>
+          <Comic number={this.state.number} comic={this.state.comic} url={this.state.url} updateComicFn={this.updateComic} shouldUpdateLock={this.state.shouldUpdateLock}/>
         </div>
       </div>
     );
